69d0c5c56d1a3f8d6e828fc1db198898f007eb56
NUR KOLILIK <46376514+kolilik20@users.noreply.github.com>
2021-11-10 22:47:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html> <!-- menandakan dokumen adalah html5 -->
+<html lang="en">
+ <head><title>CV</title>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+     <style type="text/css">
+		.konten{
+			height: 470px;
+            color:white;
+            
+		}
+        .konten2{
+			height: 700px;
+		}
+        .konten3{
+			height: 400px;
+		}
+        img{
+            width:200px;
+            height:300px;
+        }
+	</style>
+ </head>
+ <body>
+ <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+        <div class="container-fluid konten" style="background-color:#294131;">
+            <nav class="navbar navbar-expand-lg navbar-dark">
+            <a class="navbar-brand" href="dashboard.php"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="portofolio.php">Portofolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kontak.php">Contact</a>
+                </li>
+                </ul>
+            </div>
+            </nav>
+            <br><br>
+            <div class="row">
+                <div class="col" style="padding-left:100px" >
+                    <p class="lead">Hello, I'm</p>
+                    <h1 class="display-4">Nur Kolilik</h1>
+                    <h5>Fresh Graduate and Data Science Enthusiast.</h5>
+                    <hr class="my-4">
+                    <a class="btn btn-danger btn-lg" href="kontak.php" role="button">Let's Talk -></a><br><br>
+                    <a href="mailto:koiliknur@gmail.com" title = "Email" target ="_blank"/>
+                        <img align = "left" src = "img/email.png" style="width:30px;height:25px"/></a>
+                    <a href="https://wa.me/0895411138679" title = "WhatsApp" target ="_blank"/>
+                        <img align = "left" src = "img/wa.png" style="width:35px;height:26px"/></a>
+                    <a href="https://www.facebook.com/Kolilik" title = "Facebook" target ="_blank"/>
+                        <img align = "left" src = "img/fb2.png" style="width:35px;height:25px"/></a>
+                    <a href="https://instagram.com/kolilik20" title = "Instagram" target ="_blank"/>
+                        <img align = "left" src = "img/ig2.png" style="width:35px;height:26px"/></a><br><br>
+                    <a href="img/CV_Nur_Kolilik.png" download="CV_Nur_Kolilik"><h7>Download CV</h7></a>
+                </div>
+                <div class="col">
+                    <img src="img/fotobgred.jpg" class="rounded mx-auto d-block">
+                </div>        
+            </div>
+            </div>
+        <div class="container-fluid">
+            <div class="row">
+		    <div class="col-md-12 konten3" style="background-color:#386135;">
+            <div class="container" style="color:white">
+                <div class="row">
+                <div class="col-sm-12"><br>
+                    <h3 class="text-center">About
+                    </h3>
+                    <hr>
+                </div>
+                </div>
+                <div class="row">
+                <div class="col-md-12 col-sm-offset-1">
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; I am a bachelor of informatics engineering from Dian Nuswantoro University graduated in 2021,
+                        I am a fresh graduate, I understand python, R language, C language, PHP language, and SQL queries, 
+                        I am ready to work hard, learn more about data science, and work individually or in groups.
+                    </p>
+                </div>
+                </div>
+            </div>
+            </div>
+	        </div>
+        </div>
+        <div class="container-fluid">
+        <div class="row" style="color:white">
+		    <div class="col-md-12 konten2" style="background-color:#294131;"><br>
+                    <h3 class="text-center">Skills and Tools</h3>
+                    <hr>
+                    
+                    <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <div class="col">
+                            <center>
+                            <img src="img/python.png" class="card-img-top" alt="..." style="width:45px;height:45px"></center>
+                            <div class="card-body">
+                            <center><h5 class="card-title">Python Language</h5></center>
+                              </div>  
+                        </div>
+                        <div class="col">
+                            <center>
+                            <img src="img/r.png" class="card-img-top" alt="..." style="width:43px;height:43px"></center>
+                            <div class="card-body">
+                            <center><h5 class="card-title">R Language</h5></center>
+                              </div>
+                        </div>
+                        <div class="col">
+                            <center>
+                            <img src="img/c.jpg" class="card-img-top" alt="..." style="width:55px;height:43px"></center>
+                            <div class="card-body">
+                            <center><h5 class="card-title">C Language</h5></center>
+                              </div>
+                        </div>
+                        <div class="col">
+                            <center>
+                            <img src="img/php.png" class="card-img-top" alt="..." style="width:60px;height:43px"></center>
+                            <div class="card-body">
+                            <center><h5 class="card-title">PHP Language</h5></center>
+                              </div>
+                        </div>
+                        <div class="col">
+                            <center>
+                            <img src="img/sql.png" class="card-img-top" alt="..." style="width:60px;height:35px"></center>
+                            <div class="card-body">
+                            <center><h5 class="card-title">SQL</h5></center>
+                              </div>
+                        </div>
+                        <div class="col">
+                            <center>
+                            <img src="img/anaconda.png" class="card-img-top" alt="..." style="width:90px;height:45px"></center>
+                            <div class="card-body">
+                            <center><h5 class="card-title">Anaconda</h5></center>
+                              </div>
+                        </div>
+                        <div class="col">
+                            <center>
+                            <img src="img/jupiter.png" class="card-img-top" alt="..." style="width:45px;height:43px"></center>
+                            <div class="card-body">
+                            <center><h5 class="card-title">Jupyter Notebook</h5></center>
+                              </div>
+                        </div>
+                    </div>
+
+            </div>
+        </div> 
+    </div>
+ </body>
+</html>
---
+++ portofolio.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html> <!-- menandakan dokumen adalah html5 -->
+<html lang="en">
+ <head><title>CV</title>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+     <style type="text/css">
+		.konten{
+			height: 80px;
+		}
+        .konten2{
+			height: 900px;
+		}
+
+	</style>
+ </head>
+ <body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+        
+    <nav class="container-fluid navbar navbar-expand-lg navbar-dark "  style="background-color:#294131;">
+            <a class="navbar-brand" href="dashboard.php"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item ">
+                    <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="portofolio.php">Portofolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kontak.php">Contact</a>
+                </li>
+                </ul>
+            </div>
+            </nav>
+        <div class="container-fluid">
+        <div class="row">
+		    <div class="col-md-12 konten2" style="background-color:#386135;">
+            <div class="container" style="color:white">
+            <div class="row">
+                <div class="col-sm-12"><br>
+                    <h5 class="text-center">Portofolio
+                    </h5>
+                    <hr>
+                </div>
+            </div>
+            </div>
+
+                <center>
+                <table class="table table-bordered" style="color:white">
+                <caption>Portofolio</caption>
+                <br>
+                <thead>
+                    <tr>
+                    <th >No.</th>
+                    <th >Nama Portofolio</th>
+                    <th >Keterangan</th> 
+                    <th >File</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr>
+                            <td align="'left'">1.</td>
+                            <td>IMPLEMENTASI FUZZY LOGIC DALAM MENENTUKAN REWARD TERHADAP KARYAWAN
+                                MENGGUNAKAN METODE TSUKAMOTO (Study Kasus : CV MANDIRI BANGUN BERKAH)
+                            </td>
+                            <td>Web PHP Bootstrap</td>
+                            <td><a href="https://skripsi-nur-kolilik.000webhostapp.com/">https://skripsi-nur-kolilik.000webhostapp.com/</a></td>
+                        </tr>
+         				<tr>
+                            <td align="'left'">2.</td>
+                            <td>Pelatihan SQL, Python, R Language</td>
+                            <td>Sertifikat</td>
+                            <td><a href="img/Sertifikat_keahlian_nur_kolilik.jpg">file</a></td>
+                        </tr>  
+                </tbody>
+            </table></center>
+
+            </div>
+        </div>
+
+ </body>
+</html>
